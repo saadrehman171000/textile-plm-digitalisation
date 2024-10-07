3ce9b0fbd1645cd96ce32a6d98bf9dd15c781f86
Ali Shaikh <101398958+shaiiikh@users.noreply.github.com>
2024-10-08 00:40:39 +0500

    Update components-product-management.tsx

--- components/components-product-management.tsx
+++ components/components-product-management.tsx
@@ -8,6 +8,8 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { Textarea } from "@/components/ui/textarea"
 
+
+
 type Product = {
   id: number
   name: string
